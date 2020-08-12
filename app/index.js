02ae5a13b82e94cc575e8c9e25a0e646a1e962f2
Nicolas Dao <nic@neap.co>
2020-08-12 17:11:38 +1000

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,15 @@
 const { app } = require('@neap/funky')
 
+const ENV_VARS = [
+	'DB_API_ENDPOINT'
+]
+
+for (let varName of ENV_VARS)
+	if (!process.env[varName])
+		throw new Error(`Missing required environment variable 'process.env.${varName}'`)
+
+console.log(`INFO - All environment variables are properly set`)
+
 app.get('/', (req,res) => res.status(200).send(`Hello world`))
 
 app.listen({
